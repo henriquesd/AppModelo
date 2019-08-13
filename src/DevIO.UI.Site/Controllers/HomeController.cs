cf6696db11bf8feb55aa4a218889180f3df0dcbf
henriquesd <hsd.foz@gmail.com>
2019-08-13 17:26:28 -0300

    Injetando dependências no MVC

--- src/DevIO.UI.Site/Controllers/HomeController.cs
+++ src/DevIO.UI.Site/Controllers/HomeController.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevIO.UI.Site.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DevIO.UI.Site.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPedidoRepository _pedidoRepository;
+
+        // O MVC vai passar para o construtor, conforme está configurado no Startup (no ConfigureServices);
+        public HomeController(IPedidoRepository pedidoRepository)
+        {
+            _pedidoRepository = pedidoRepository;
+        }
+
         public IActionResult Index()
         {
+            var pedido = _pedidoRepository.ObterPedido();
+
             return View();
         }
+
+        // Se por algum motivo você não possa mecher no construtor, é possível injetar depedência através do
+        // atributo FromServices (mas a melhor forma de injetar uma dependência, é no construtor). Por exemplo:
+        //public IActionResult Index([FromServices] IPedidoRepository _pedidoRepository)
+        //{
+        //    var pedido = _pedidoRepository.ObterPedido();
+
+        //    return View();
+        //}
+
     }
 }
